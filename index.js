42535a7b08cea981c6cd7f6e44b0e2dfb7cd418f
Martin Kruusement <martinkruusement@gmail.com>
2022-05-18 21:00:35 +0300

    all tests pass

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 /* eslint no-unused-expressions: "warn" */
 const cases = require('./load-tests.js')
 
-function solution (A) {
-  let input = A
-  let output = null
-  output
+// var findMedianSortedArrays = function (nums1, nums2) {
+function solution (input) {
+  let nums1 = input[0]
+  let nums2 = input[1]
 
-  return output
+  nums1 = (Array.isArray(nums1) && nums1.length) ? nums1 : []
+  nums2 = (Array.isArray(nums2) && nums2.length) ? nums2 : []
+
+  let numbers = [...nums1, ...nums2].map(n => parseFloat(n)).sort((a, b) => a - b)
+  let isEven = numbers.length % 2 === 0
+  let startCut = Math.floor(numbers.length / 2)
+  let endCut = startCut + 1
+  if (isEven) {
+    startCut = Math.floor(numbers.length / 2) - 1
+  }
+
+  let medianSource = numbers.slice(startCut, endCut)
+
+  if (medianSource.length === 1) {
+    return medianSource[0]
+  }
+  if (medianSource.length === 2) {
+    return (medianSource[0] + medianSource[1]) / 2
+  }
+  return 0
 }
 
 /* eslint-disable no-unused-expressions */
